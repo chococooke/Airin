cbeb653881412b687e241ea9e434fa37b074e8aa
chococooke * web3 <pro.bhimsen@gmail.com>
2024-06-13 23:03:07 +0530

    Add new button for create-post form popup

--- frontend/src/Containers/Sidebar.jsx
+++ frontend/src/Containers/Sidebar.jsx
@@ -1,8 +1,21 @@
 import { Link } from "react-router-dom";
+import CreatePost from "../Pages/CreatePost";
+import { useState } from "react";
 
 const Sidebar = () => {
+    const [showCreatePostForm, setShowCreatePostForm] = useState(false);
+
+    const toggleShowCreatePostForm = (e) => {
+        e.preventDefault();
+
+        setShowCreatePostForm(!showCreatePostForm)
+    }
+
     return (
         <>
+            {
+                showCreatePostForm && <CreatePost close={toggleShowCreatePostForm} />
+            }
             <div className="sidebar">
                 <nav className="sidebar__nav">
                     <ul className="sidebar__nav-list">
@@ -21,7 +34,15 @@ const Sidebar = () => {
                         <li className="sidebar__nav-list--item">
                             <Link to={'/settings'}>Settings</Link>
                         </li>
+                        <li className="sidebar__nav-list--item">
+                            <button
+                                onClick={(e) => toggleShowCreatePostForm(e)}
+                            >
+                                Create Post
+                            </button>
+                        </li>
                     </ul>
+                    <button className="btn" >Log out</button>
                 </nav>
             </div>
         </>
